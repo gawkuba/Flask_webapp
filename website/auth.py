994001b2db144a4b2b58700e06629d5aeb0278d5
gawelczak <gawkuba@gmail.com>
2025-12-10 00:17:06 +0100

    Work on: HTTP requests, handling POST, messages flashing, SQLAlchemy setup, db models, FK relationships

--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,35 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    data = request.form
+    print(data)
     return render_template('login.html', boolean=True)
 
 @auth.route('/logout')
 def logout():
     return "<p>Logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('first_name')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters', category='error')
+        elif len(first_name) < 2:
+            flash('First must be greater than 2 characters', category='error')
+        elif password1 != password2:
+            flash('Passwords do not match', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 8 characters', category='error')
+        else:
+            #add user to database
+            flash('Account created!', category='success')
     return render_template('sign_up.html')
